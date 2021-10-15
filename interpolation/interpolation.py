c6e5021ab475a020ecef7193da3cccdfede00868
David Emms <david_emms@hotmail.com>
2021-10-15 16:00:41 +0100

    Read input file

--- interpolation/interpolation.py
+++ interpolation/interpolation.py
@@ -6,18 +6,49 @@ import csv
 import argparse
 
 
-def main(input, output):
+def main(fn_input, fn_output):
     """
     Interpolate values in csv file
     Args:
-        input - filename for input CSV file
-        input - filename for output CSV file
+        fn_input - filename for input CSV file
+        fn_output - filename for output CSV file
     Returns:
         None
     """
+    # read file
+    try:
+        data = read_from_file(fn_input)
+    except Exception as e:
+        print(str(e))
+        print("Exiting...")
+        return
 
-def return2():
-    return 2
+    # write & fill in missing values
+    print("ERROR: Interpolation has not been implemented yet")
+
+
+def read_from_file(fn_input):
+    """
+    Load a file from CSV
+    Args:
+        fn_input - input filename
+    Returns:
+        data - list of lists of floats/None for present/absent data
+    """
+    with open(fn_input, 'r') as infile:
+        reader = csv.reader(infile)
+        data = []
+        for row in reader:
+            data.append([])
+            for value in row:
+                if value == 'nan':
+                    data[-1].append(None)
+                else:
+                    try:
+                        data[-1].append(float(value))
+                    except:
+                        raise Exception("ERROR: unexpected text in input file: '%s'" % str(value))
+        return data
 
 
 if __name__ == "__main__":
--- test/test_interpolation.py
+++ test/test_interpolation.py
@@ -1,13 +1,85 @@
 #/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
+import tempfile
 import unittest
+
 from interpolation import interpolation
 
 
-class TestInterpolation(unittest.TestCase):
-    def test_return2(self):
-        self.assertEqual(interpolation.return2(), 2)
+# directory containing unittest data
+d_data = os.path.basename(__file__) + os.sep + "test_data" + os.sep
 
+
+class TestInterpolation(unittest.TestCase):
+    def test_read_from_file_2x2(self):
+        with tempfile.TemporaryDirectory() as dir_test:
+            # arrange
+            fn = dir_test + os.sep + "data.csv"
+            with open(fn, 'w') as outfile:
+                outfile.write("7.5,9.1\n")
+                outfile.write("3.2,1.5\n")
+        
+            # act
+            data = interpolation.read_from_file(fn)
+
+        # assert
+        self.assertEqual(2, len(data))
+        self.assertListEqual([7.5, 9.1], data[0])
+        self.assertListEqual([3.2, 1.5], data[1])
+
+
+    def test_read_from_file_3x3(self):
+        with tempfile.TemporaryDirectory() as dir_test:
+            # arrange
+            fn = dir_test + os.sep + "data.csv"
+            with open(fn, 'w') as outfile:
+                outfile.write("1,2,3\n")
+                outfile.write("4,5,6\n")
+                outfile.write("7,8,9\n")
+        
+            # act
+            data = interpolation.read_from_file(fn)
+
+        # assert
+        self.assertEqual(3, len(data))
+        self.assertListEqual([1.0, 2.0, 3.0], data[0])
+        self.assertListEqual([4.0, 5.0, 6.0], data[1])
+        self.assertListEqual([7.0, 8.0, 9.0], data[2])
+
+
+    def test_read_from_file_nan(self):
+        with tempfile.TemporaryDirectory() as dir_test:
+            # arrange
+            fn = dir_test + os.sep + "data.csv"
+            with open(fn, 'w') as outfile:
+                outfile.write("1,nan\n")
+                outfile.write("nan,2\n")
+                outfile.write("3,4\n")
+        
+            # act
+            data = interpolation.read_from_file(fn)
+
+        # assert
+        self.assertEqual(3, len(data))
+        self.assertListEqual([1., None], data[0])
+        self.assertListEqual([None, 2.], data[1])
+        self.assertListEqual([3., 4.], data[2])
+
+
+    def test_read_from_file_format_error(self):
+        with tempfile.TemporaryDirectory() as dir_test:
+            # arrange
+            fn = dir_test + os.sep + "data.csv"
+            with open(fn, 'w') as outfile:
+                outfile.write("1,other\n")
+                outfile.write("nan,2\n")
+        
+            # act
+            with self.assertRaises(Exception) as context:
+                interpolation.read_from_file(fn)
+
+            self.assertTrue("ERROR: unexpected text in input file: 'other'" in str(context.exception))
 
 
 if __name__ == '__main__':
